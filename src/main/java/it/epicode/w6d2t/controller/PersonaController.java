e5311a120f5d782c03940b262118c17bc0155860
TeachVN <vinnap1976@gmail.com>
2024-02-13 13:08:24 +0100

    gestione del metodo getMapping con ResponseEntity, e altri mapping nel controller

--- src/main/java/it/epicode/w6d2t/controller/PersonaController.java
+++ src/main/java/it/epicode/w6d2t/controller/PersonaController.java
@@ -3,9 +3,12 @@ package it.epicode.w6d2t.controller;
 import it.epicode.w6d2t.model.Persona;
 import it.epicode.w6d2t.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 //@RequestMapping("/api")
@@ -22,8 +25,27 @@ public class PersonaController {
         return personaService.cercaPersonaPerId(id);
     }
     @GetMapping("/persone/{id}")
-    public Persona getPersonaByIdPathVariable(@PathVariable int id){
-        return personaService.cercaPersonaPerId(id);
+    public ResponseEntity<Persona> getPersonaByIdPathVariable(@PathVariable int id){
+        try {
+            Persona p = personaService.cercaPersonaPerId(id);
+            return new ResponseEntity<Persona>(p, HttpStatus.OK);
+        }
+        catch (NoSuchElementException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @PostMapping("/persone")
+    public void savePersona(@RequestBody Persona persona){
+        personaService.salvaPersona(persona);
+    }
+    @PutMapping("/persone/{id}")
+    public Persona updatePersona(@PathVariable int id, @RequestBody Persona persona){
+        return personaService.aggiornaPersona(id, persona);
+    }
+    @DeleteMapping("/persone/{id}")
+    public void deletePersona(@PathVariable int id){
+        personaService.cancellaPersona(id);
     }
 
 
